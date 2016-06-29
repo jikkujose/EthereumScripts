a79dee05e9facb667231ea4e3e2a23022f8ca18e
Jikku Jose <jikkujose@gmail.com>
2016-06-29 15:00:51 +0530

    Fix bug in transact

--- transact.js
+++ transact.js
@@ -1,22 +1,23 @@
 var transact = function(from, to, amount){
   var object = {
-    from: from,
-    to: to,
-    amount: amount,
-    charge: 2000000000000000,
-    denomination: 'ether',
-    value: function(){
-      return web3.fromWei(this.amount, denomination);
+    payload: {
+      from: from,
+      to: to,
+      amount: amount,
+      charge: 2000000000000000,
+      denomination: 'ether',
+      value: function() {
+        return parseInt(web3.toWei(
+          this.amount,
+          this.denomination
+        ));
+      }
     },
     availableBalance: function() {
-      return eth.getBalance(from) - this.charge;
+      return eth.getBalance(from) - this.payload.charge;
     },
     transfer: function() {
-      return eth.sendTransaction({
-            from: this.from,
-            to: this.to,
-            value: this.value()
-          });
+      return eth.sendTransaction(this.payload);
     },
     transferAll: function(){
       return eth.sendTransaction({
