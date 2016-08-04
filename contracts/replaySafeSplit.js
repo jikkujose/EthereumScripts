6a6078379b2f890b6ec3367ccda35e4fef93e7d7
Jikku Jose <jikkujose@gmail.com>
2016-08-04 20:47:24 +0530

    Add helper functions to replaySafeSplit contract

--- contracts/replaySafeSplit.js
+++ contracts/replaySafeSplit.js
@@ -23,6 +23,9 @@ contracts.replaySafeSplit = {
     }
   ],
   address: '0xaa1a6e3e6ef20068f7f8d8c835d2d22fd5116444',
+  preForkAddress: null,
+  ethAddress: null,
+  etcAddress: null,
   contract: function() {
     return(
       web3.eth.contract(this.abi)
@@ -32,5 +35,36 @@ contracts.replaySafeSplit = {
     return(
       this.contract().at(this.address)
     );
+  },
+  setAddresses: function(preForkAddress, ethAddress, etcAddress) {
+    this.preForkAddress = preForkAddress;
+    this.ethAddress = ethAddress;
+    this.etcAddress = etcAddress;
+  },
+  splitSafely: function(value) {
+    if(
+      !this.preForkAddress ||
+        !this.ethAddress ||
+        !this.etcAddress
+    ) {
+      throw('setAddresses(...) needs to be called before splitting!')
+    }
+    return(
+      this
+      .instance()
+      .split
+      .sendTransaction(
+        this.ethAddress,
+        this.etcAddress,
+        this.getPayload(value)
+      )
+    );
+  },
+  getPayload: function(value) {
+    return({
+      from: this.preForkAddress,
+      to: this.address,
+      value: value
+    });
   },
 }
